4e90d637889ef5dd0bec6dd7cd3a3198b0123f00
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2021-04-16 10:16:40 -0500

    Rework of catalog; waiting on database schema/query

--- src/commands/Catalog.js
+++ src/commands/Catalog.js
@@ -1,24 +1,21 @@
 import chalk from 'chalk';
+import { group } from "d3-array";
 
 const Catalog = {
   name: 'catalog',
   description: "Displays most recent events in event catalog.",
   execute({ client }) {
+    // Query from database
+    // let data =
+    // Create array from nested InternMap
+    // let catalog = Array.from(d3.group(data, d => d.format, d => d.date));
     try {
-      // return {
-      //   title: 'Catalog',
-      //   description:'',
-      //   fields: [
-      //     {
-      //       name: '',
-      //       value: '',
-      //       inline: false,
-      //     },
-      //   ],
-      //   timestamp: new Date(),
-      //   footer: {
-      //     text: '/catalog'
-      //   };
+      return {
+        title: 'Catalog',
+        description:'',
+        // Map array from catalog object
+        // fields: [],
+      };
     } catch (error) {
       // Send full error stack to console
       console.error(chalk.red(`/catalog >> ${error.stack}`));
